9d8e6a99d2379da9df886a462e51fcdf1ff8fbb7
Tristian Flanagan <tflanagan@datacollaborative.com>
2015-09-14 18:33:57 -0400

    module: fix error offset on first line errors via nl

--- src/node.js
+++ src/node.js
@@ -930,7 +930,7 @@
   };
 
   NativeModule.wrapper = [
-    '(function (exports, require, module, __filename, __dirname) { ',
+    '(function (exports, require, module, __filename, __dirname) {\n',
     '\n});'
   ];
 
